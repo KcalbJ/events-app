cca0f259ce7aacf0580636b9869804826867a747
kcalbj <jblack.create@gmail.com>
2024-07-13 14:09:49 +0100

    switched to typescript and added basic google auth

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default async function Home() {
+
+  return (
+    <main className="flex flex-col items-center gap-6 px-3 py-10">
+    <h1> Events app</h1>
+  </main>
+  );
+}
