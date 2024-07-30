fd39bee71bcd4d35a8546c6f8826ac88aadb7a1a
kcalbj <jblack.create@gmail.com>
2024-07-30 12:58:12 +0100

    fixed path

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order'
 
 
+
 export async function POST(request: Request) {
   const body = await request.text()
 
